3b074c7f33cf57922a22f13daf5bdeb7f71517c6
Thanh <tnansci@gmail.com>
2021-10-09 14:32:37 -0400

    Completed checkBalance and deposit methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
 
@@ -13,5 +14,38 @@ public class Main {
 	    id = iInput.next();
 
 	    System.out.println(String.format("Hello %s, ID number %s", name, id));
+
+	    Account person = new Account(name, id);
+		person.deposit();
+
+//		Account one = new Account("Tan", "123");
+//		one.checkBalance();
+//		double test = 10000;
+//		System.out.println(String.format("Your current balance is: $%,.2f", test));
+
     }
+}
+
+class Account {
+	String aName, aID;
+	double balance = 10000;
+
+	Account(String name, String id) {
+		aName = name;
+		aID = id;
+
+	}
+
+	void checkBalance() {
+		System.out.println(String.format("Your current balance is: $%,.2f", balance));
+	}
+
+	void deposit() {
+		System.out.print("Enter the amount to deposit: ");
+		Scanner inputAmount = new Scanner(System.in);
+		double amount = inputAmount.nextDouble();
+		balance += amount;
+		System.out.println(String.format("Deposited $%,.2f successfully.", amount));
+	}
+
 }
