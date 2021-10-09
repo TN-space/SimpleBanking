9820eda836d74f165dbe19de3647f0e964a6bf1f
Thanh <tnansci@gmail.com>
2021-10-09 13:20:28 -0400

    Taking user inputs for name and id

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	String name, id;
+	    System.out.print("Please enter your name: ");
+	    Scanner nInput = new Scanner(System.in);
+	    name = nInput.next();
+	    System.out.print("Enter your ID: ");
+	    Scanner iInput = new Scanner(System.in);
+	    id = iInput.next();
+
+	    System.out.println(String.format("Hello %s, ID number %s", name, id));
     }
 }
